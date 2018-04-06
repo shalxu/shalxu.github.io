19ed0e2cf320e54e306e28be90314a92395ca4ac
Shal <shalxu@qq.com>
2018-04-06 12:15:35 -0700

    a lot of styles
    Now mobile-friendly

--- app.js
+++ app.js
@@ -1,71 +1,57 @@
-var app = angular.module('app', ["ui.bootstrap","ngRoute","ngAnimate","ngTouch"]);
+ var app = angular.module('app', ["ui.bootstrap", "ngRoute", "ngAnimate", "ngTouch"]);
 
-app.config(function($routeProvider) {
-    $routeProvider
-    .when("/home", {
-        templateUrl : "home.html"
-    })
-    .when("/faq", {
-        templateUrl : "faq.html"
-    }) 
-    .when("/rsvp", {
-        templateUrl : "rsvp.html"
-    })
-    .when("/visiting", {
-    templateUrl : "visiting.html"
-    })
+ app.config(function($routeProvider) {
+     $routeProvider
+         .when("/home", {
+             templateUrl: "home.html"
+         })
+         .when("/faq", {
+             templateUrl: "faq.html"
+         })
+         .when("/rsvp", {
+             templateUrl: "rsvp.html"
+         })
+         .when("/visiting", {
+             templateUrl: "visiting.html"
+         })
 
-   .when("/aboutPage", {
-        templateUrl : "aboutPage.html"
-    })
-    .otherwise({
-    redirectTo: "/aboutMe"
-  });
-});
+         .when("/aboutPage", {
+             templateUrl: "aboutPage.html"
+         })
+         .otherwise({
+             redirectTo: "/home"
+         });
+ });
 
-app.controller('MainController', function ($animate,$scope) {
-	 $animate.enabled(true); 
+ app.controller('MainController', function($animate, $scope, $location) {
+     $animate.enabled(true);
 
-   $scope.slides=[{
-      image: "images/shal.JPG",
-      text: "Shal Xu",
-      button:"About Me",
-      link:"#home",
-      index:0
-    },
-    {
-   	 image: "images/pedals.jpeg",
-      text: "4+ Years of Watercolor, Charcoal, Colored Pencil, Pen... ",
-      button:"Browse My Art Portfolio HERE",
-      link:"http://shalxu.tumblr.com",
-      index:1
-    },
-      {
-    	image: "images/code.jpg",
-      text: "A Visual Programmer",
-      button:"Take a Look at My Other Projects Too!",
-      link:"#projects",
-      index:2
-    }];
+     $scope.nav = $location.path().split('/')[1] || 'home';
 
-    $scope.nav="home";
-   $scope.checkSlideActive=function(index){
-      if(index==0){
-        console.log(index);
-        return "item active";
-      }
-      else
-        return "item";
-    }
+     $scope.checkActive = function(item) {
+         if (item == $scope.nav)
+             return "active";
+         else
+             return;
+     }
+     $scope.setActive = function(item) {
+         $scope.nav = item;
+     }
 
-    $scope.checkActive=function(item){
-      if(item==$scope.nav)
-        return "active";
-      else
-        return;
-    }
-     $scope.setActive=function(item){
-      $scope.nav=item;
-    }
+     document.getElementById("backgroundMusic").volume = 0.1;
 
-});
+ });
+
+ //Parallax 
+ function simpleParallax() {
+     //This variable is storing the distance scrolled
+     var scrolled = $(window).scrollTop() + 1;
+
+     //Every element with the class "scroll" will have parallax background 
+     //Change the "0.3" for adjusting scroll speed.
+     $('.parallax').css('background-position', '0' + -(scrolled * 0.3) + 'px');
+ }
+ //Everytime we scroll, it will fire the function
+ $(window).scroll(function(e) {
+     simpleParallax();
+ });
