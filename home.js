99e154ea7e635e8e14ae94d56a4e51bfbe768fa0
Shal <shalxu@qq.com>
2018-04-07 16:20:32 -0700

    countdown temp

---
+++ home.js
@@ -0,0 +1,21 @@
+	app.controller('HomeController', function ($animate,$scope) {
+	FlipClock.Lang.Custom = { days:'Days', hours:'Hours', minutes:'Minutes', seconds:'Seconds' };
+	var opts = {
+		clockFace: 'DailyCounter',
+		countdown: true,
+		language: 'Custom'
+	};
+	opts.classes = {
+		active: 'flip-clock-active',
+		before: 'flip-clock-before',
+		divider: 'flip-clock-divider',
+		dot: 'flip-clock-dot',
+		label: 'flip-clock-label',
+		flip: 'flip',
+		play: 'play',
+		wrapper: 'flip-clock-small-wrapper'
+	};  
+	var countdown = 1532210400 - ((new Date().getTime())/1000); // from: 07/21/2018 03:00 pm -0700
+	countdown = Math.max(1, countdown);
+	$('.countdown').FlipClock(countdown, opts);
+});
